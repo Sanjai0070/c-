bedfc6ec9c17def17c2f82a04fefab164f759089
Sanjai0070 <161039013+Sanjai0070@users.noreply.github.com>
2024-04-02 09:51:51 +0530

    Add files via upload

---
+++ area (fun overloading) c++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+void area(int side) {
+    int squareArea = side * side;
+    cout << "The Area Of Square Value is: " << squareArea << endl;
+}
+
+
+void area(int length, int width) {
+    int rectangleArea = length * width;
+    cout << "The Area Of the Rectangle Value is: " << rectangleArea << endl;
+}
+
+int main() {
+    int side, length, width;
+
+    cout << "Enter the side length of the square: ";
+    cin >> side;
+
+    cout << "Enter the length and width of the rectangle: ";
+    cin >> length >> width;
+
+    area(side); 
+    area(length, width); 
+
+    return 0;
+}
