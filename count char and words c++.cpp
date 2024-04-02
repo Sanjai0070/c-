cf965d9fdc5dd8c9389f24fab5957bbf4bbae7e5
Sanjai0070 <161039013+Sanjai0070@users.noreply.github.com>
2024-04-02 09:27:42 +0530

    Add files via upload

---
+++ count char and words c++.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string text;
+    cout << "Enter the text: ";
+    getline(cin, text);
+
+    int wordCount = 0;
+    int charCount = 0;
+
+    for (char c : text) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            wordCount++;
+        }
+        charCount++;
+    }
+
+    wordCount++;
+
+    cout << "Number of words: " << wordCount << endl;
+    cout << "Number of characters: " << charCount << endl;
+
+    return 0;
+}
